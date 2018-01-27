019f2d81f0d331d50c3a9f49d1db533ade739b91
Y. Z. Chen <754097987@qq.com>
2018-01-27 16:43:21 +0800

    [Update] A basic logic and a sample.

--- app/src/main/java/com/geno/mutextview/MainActivity.kt
+++ app/src/main/java/com/geno/mutextview/MainActivity.kt
@@ -1,14 +1,35 @@
 package com.geno.mutextview
 
 import android.app.Activity
+import android.app.AlertDialog
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Base64
+import android.view.View
+import android.widget.EditText
+import android.widget.LinearLayout
+import android.widget.ScrollView
+import android.widget.TextView
 
 class MainActivity : Activity()
 {
-
     override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(EthTextView(this).apply {
+            relation = object : EthTextView.OnEditTextChangedListener
+            {
+                override fun onEditText0Changed(editText0: EditText, editText1: EditText)
+                {
+                    editText1.setText("${editText0.text}1")
+                }
+
+                override fun onEditText1Changed(editText0: EditText, editText1: EditText)
+                {
+                    editText0.setText("${editText1.text}2")
+                }
+            }
+        })
     }
 }
---
+++ library/src/main/java/com/geno/mutextview/EthTextView.kt
@@ -0,0 +1,80 @@
+package com.geno.mutextview
+
+import android.content.Context
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.EditText
+import android.widget.LinearLayout
+
+/**
+ * Created by geno1024 on 2018-01-27 10:25.
+ *
+ * A layout combined with two LinearLayout which are influenced with each other, avoided a dead
+ * loop of calling each other's TextWatcher.
+ *
+ * "Eth" comes from the chemical prefix of organics, are used to mean there are two carbons.
+ */
+
+class EthTextView(context: Context, val editText0: EditText, val editText1: EditText): LinearLayout(context)
+{
+    /**
+     * Boolean array for status to build "mutex lock".
+     */
+    private val status = arrayOf(false, false)
+
+    var relation: EthTextView.OnEditTextChangedListener = object : OnEditTextChangedListener
+    {
+        override fun onEditText0Changed(editText0: EditText, editText1: EditText) {}
+        override fun onEditText1Changed(editText0: EditText, editText1: EditText) {}
+    }
+
+    constructor(context: Context): this(context, EditText(context), EditText(context))
+    {
+        orientation = LinearLayout.VERTICAL
+        addView(editText0, LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT)
+        addView(editText1, LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT)
+    }
+
+    init
+    {
+        editText0.addTextChangedListener(object : TextWatcher
+        {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int)
+            {
+                status[0] = true
+            }
+
+            override fun afterTextChanged(s: Editable?)
+            {
+                if (status[0] && !status[1])
+                    relation.onEditText0Changed(editText0, editText1)
+                status[0] = false
+            }
+        })
+
+        editText1.addTextChangedListener(object : TextWatcher
+        {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int)
+            {
+                status[1] = true
+            }
+
+            override fun afterTextChanged(s: Editable?)
+            {
+                if (!status[0] && status[1])
+                    relation.onEditText1Changed(editText0, editText1)
+                status[1] = false
+            }
+        })
+    }
+
+    interface OnEditTextChangedListener
+    {
+        fun onEditText0Changed(editText0: EditText, editText1: EditText)
+        fun onEditText1Changed(editText0: EditText, editText1: EditText)
+    }
+}
