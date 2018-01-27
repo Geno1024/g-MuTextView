6797ef0083abed917a59950c601e4d232a406675
Y. Z. Chen <754097987@qq.com>
2018-01-27 09:34:59 +0800

    {INIT} Initialize. What's the meaning of atomical commit?

---
+++ library/src/main/java/com/geno/mutextview/MuTextView.kt
@@ -0,0 +1,13 @@
+package com.geno.mutextview
+
+import android.content.Context
+import android.widget.TextView
+
+/**
+* Created by geno1024 on 2018-01-27 09:30.
+*/
+
+class MuTextView(context: Context): TextView(context)
+{
+
+}
---
+++ library/src/main/java/com/geno/mutextview/MuTextViewGroup.kt
@@ -0,0 +1,14 @@
+package com.geno.mutextview
+
+import android.content.Context
+import android.view.ViewGroup
+import android.widget.LinearLayout
+
+/**
+* Created by geno1024 on 2018-01-27 09:32.
+*/
+
+class MuTextViewGroup(context: Context): LinearLayout(context)
+{
+
+}
